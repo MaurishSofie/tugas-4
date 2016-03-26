91a992a0e4a16093fac5fbc1a6faad4c5c5b6e26
Maurish Sofie <maurish09@gmail.com>
2016-03-26 15:45:42 +0700

    encapsulation 1 done

---
+++ src/tugas/pkg4/Student.java
@@ -0,0 +1,17 @@
+package tugas.pkg4;
+public class Student {
+    public String name;
+    public int mark;
+    public void setName(String n) {
+        name = n;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setMark(int m) {
+        mark = m;
+    }
+    public int getMark() {
+        return mark;
+    }
+}
