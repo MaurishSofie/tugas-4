2b7d9ac20d69898bf5c499a35d9538314576d217
Maurish Sofie <maurish09@gmail.com>
2016-03-26 23:14:00 +0700

    source code done

--- src/tugas/pkg4/BukuAlamat.java
+++ src/tugas/pkg4/BukuAlamat.java
@@ -1,6 +1,14 @@
 package tugas.pkg4;
+import java.util.Scanner;
 public class BukuAlamat {
     private String nama, alamat, telepon, email;
+    Scanner input = new Scanner(System.in);
+    public BukuAlamat (){
+        nama = "";
+        alamat = "";
+        telepon = "";
+        email = "";
+    }
     
     public BukuAlamat (String n, String a, String t, String e){
         nama = n;
@@ -13,32 +21,77 @@ public class BukuAlamat {
         return nama;
     }
 
-    public void setNama(String n) {
-        nama = n;
+    public void setNama() {
+       
+        System.out.print("Nama\t: ");
+        nama = input.next();
     }
 
     public String getAlamat() {
         return alamat;
     }
 
-    public void setAlamat(String a) {
-        alamat = a;
+    public void setAlamat() {
+       
+        System.out.print("Alamat\t: ");
+        alamat = input.next();
     }
 
     public String getTelepon() {
         return telepon;
     }
 
-    public void setTelepon(String t) {
-        telepon = t;
+    public void setTelepon() {
+        
+        System.out.print("Telepon\t: ");
+        telepon = input.next();
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String e) {
-        email = e;
+    public void setEmail() {
+        
+        System.out.print("E-Mail\t: ");
+        email = input.next();
     }
     
+    public static void displayBuku (BukuAlamat[] buku){
+        for (int i = 0; i < buku.length; i++){
+            System.out.println("Data Ke-"+(i+1));
+            if(buku[i].getNama().equals(""))
+                System.out.println("Deleted");
+            else {
+                System.out.println("Nama\t : "+buku[i].getNama()+"\t");
+                System.out.println("Alamat\t : "+buku[i].getAlamat()+"\t");
+                System.out.println("Telepon\t : "+buku[i].getTelepon()+"\t");
+                System.out.println("E-Mail\t : "+buku[i].getEmail()+"\t");
+            }
+        }
+    }
+    
+    public static void inputBuku (BukuAlamat[] buku, int i){
+        buku[i] = new BukuAlamat();
+        buku[i].setNama();
+        buku[i].setAlamat();
+        buku[i].setTelepon();
+        buku[i].setEmail();
+    }
+    
+    public static void deleteBuku (BukuAlamat[] buku, int x){
+        --x;
+        buku[x] = new BukuAlamat();
+        System.out.println("");
+    }
+    
+    public static void editBuku (BukuAlamat[] buku, int x){
+        --x;
+        buku[x] = new BukuAlamat();
+        buku[x].setNama();
+        buku[x].setAlamat();
+        buku[x].setTelepon();
+        buku[x].setEmail();
+        System.out.println("");
+    }
 }
--- src/tugas/pkg4/BukuMain.java
+++ src/tugas/pkg4/BukuMain.java
@@ -1,37 +1,60 @@
 package tugas.pkg4;
+import java.util.Arrays;
 import java.util.Scanner;
 public class BukuMain {
 public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    System.out.println("\tAPLIKASI BUKU ALAMAT MORIS");
+    BukuAlamat[] buku = new BukuAlamat[0];
+    int i=0;
+    
+    System.out.println("APLIKASI BUKU ALAMAT MORIS");
     for (int j = 1; j > 0; j++){
-        System.out.println("\t\tMenu");
+        System.out.println("==========================");
+        System.out.println("\tMenu");
         System.out.println("1. Input Data");
-        System.out.println("2. Lihat Data");
+        System.out.println("2. Edit Data");
         System.out.println("3. Delete Data");
-        System.out.println("4. Exit");
+        System.out.println("4. Lihat Data");
+        System.out.println("5. Exit");
         System.out.print("Masukkan pilihan anda : ");
         int pil = input.nextInt();
+        System.out.println("==========================");
         switch(pil){
             case 1 :
-                System.out.println("Input Data");
-                System.out.print("Nama Lengkap : ");
-                
+                System.out.println("\t>>Input Data<<");
+                buku = Arrays.copyOf(buku, buku.length + 1);
+                BukuAlamat.inputBuku(buku, i);
+                i++;
                 break;
             case 2 :
-                System.out.println("Lihat Data");
+                System.out.println("\t>>Edit Data<<");
+                BukuAlamat.displayBuku(buku);
+                System.out.print("Edit data nomor : ");
+                i = input.nextInt();
+                if ((i-1)<buku.length){
+                    BukuAlamat.editBuku(buku, i);
+                } else 
+                    System.out.println("Data tidak tepat");;
                 break;
             case 3 :
-                System.out.println("Delete Data");
+                System.out.println("\t>>Delete Data<<");
+                BukuAlamat.displayBuku(buku);
+                System.out.print("Delete data nomor : ");
+                i = input.nextInt();
+                BukuAlamat.deleteBuku(buku, i);
                 break;
             case 4 :
+                System.out.println("\t>>Lihat Data<<");
+                BukuAlamat.displayBuku(buku);
+                break;
+            case 5 :
                 System.out.println("Terimakasih telah menggunakan Program ini . . . ");
                 System.exit(0);
                 break;
             default :
                 System.out.println("Pilihan yang anda masukkan salah");
         } 
-        System.out.println("Apakah Anda Ingin Kembali ke Menu ? <y/n>");
+        System.out.print("Apakah Anda Ingin Kembali ke Menu ? <y/n> : ");
         String y = input.next();
             if ("y".equals(y)){
             } else if ("n".equals(y)){
